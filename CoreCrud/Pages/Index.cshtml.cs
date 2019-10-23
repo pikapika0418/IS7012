5d7aa9fa79236391b533f5408d1dcb82a1165036
Anjana Ramji <ramjiaa@mail.uc.edu>
2019-10-23 02:43:38 -0400

    Extra Credit

--- CoreCrud/Pages/Index.cshtml.cs
+++ CoreCrud/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreCrud.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,20 @@ namespace CoreCrud.Pages
 {
     public class IndexModel : PageModel
     {
+        private CoreCrudContext _context;
+        public IndexModel(CoreCrudContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Country> Countries { get; set; }
+        public ICollection<Athlete> Athletes { get; set; }
+        public int AthleteCount { get; set; }
         public void OnGet()
         {
+            Countries = _context.Country.ToList();
+            Athletes = _context.Athlete.ToList();
+
+            AthleteCount = Athletes.Count();
 
         }
     }
